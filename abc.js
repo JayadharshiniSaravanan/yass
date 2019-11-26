b34c3836a33fcc068cf49bdb057bf98659d7696d
JayadharshiniSaravanan <56319534+JayadharshiniSaravanan@users.noreply.github.com>
2019-11-26 23:45:00 +0530

    again a new final

---
+++ abc.js
@@ -0,0 +1,11 @@
+function call(){
+  swal(
+    {
+      title:'sweet!',
+      text:'model image',
+      imageUrl:'https://data.whicdn.com/images/295658437/superthumb.jpg?t=1503787060',
+      imageAlt:'custom image',
+      confirmButtonText:'cool'
+    }
+  )
+}
